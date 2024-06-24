590cde528a83a2893d65ceab7cd0dee9a476c5b5
arigunawanj <arigunawanjatmiko@gmail.com>
2024-06-24 14:59:53 +0700

    Hari 11 - Data Fetching di NextJS

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,13 +1,28 @@
 import Layout from "@/layout";
 import { useRouter } from "next/router";
-  
-export default function UsersByName() {
- const router = useRouter();
- const { id } = router?.query;
- 
- return (
-  <Layout>
-   <p>Users by Name {id}</p>
-  </Layout>
- );
+
+export async function getStaticProps({ params }) {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const repo = await res.json()
+    return { props: { repo } }
+}
+
+export async function getStaticPaths() {
+    const paths = [
+        { params: { id: '70107786' } },
+    ];
+    
+    return { paths, fallback: false };
+}
+
+export default function UsersByName({ repo }) {
+    const router = useRouter();
+    const { id } = router.query;
+
+    return (
+        <Layout>
+            <p>Users by ID {id}</p>
+            <p>Repo Name: {repo.name}</p>
+        </Layout>
+    );
 }
