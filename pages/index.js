581bbee5d530a620f44c44c30d563a83bea0cb50
arigunawanj <arigunawanjatmiko@gmail.com>
2024-06-25 21:52:14 +0700

    Hari 12 - Implementasi API Crud di NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Layout from "@/layout";
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function Main({ children}) {
   return (
+    <ChakraProvider>
     <div>
       <Layout>
       <p>Home</p>
       </Layout>
     </div>
+    </ChakraProvider>
   );
 }
---
+++ pages/notes/index.js
@@ -0,0 +1,80 @@
+import dynamic from "next/dynamic";
+import {
+    Box,
+    Flex,
+    Grid,
+    GridItem,
+    Card,
+    CardBody,
+    CardHeader,
+    CardFooter,
+    Heading,
+    Text,
+    Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const LayoutComponent = dynamic(() => import("@/layout"));
+
+export default function Notes() {
+    const router = useRouter();
+    const [notes, setNotes] = useState();
+
+    useEffect(() => {
+        async function fetchingData() {
+            const res = await fetch("https://service.pace-unv.cloud/api/notes");
+            const listNotes = await res.json();
+            setNotes(listNotes);
+        }
+        fetchingData();
+    }, []);
+
+    return (
+        <>
+            <LayoutComponent metaTitle="Notes">
+                <Box padding="5">
+                    <Flex justifyContent="end">
+                        <Button
+                            colorScheme="blue"
+                            onClick={() => router.push("/notes/add")}
+                        >
+                            Add Notes
+                        </Button>
+                    </Flex>
+                    <Flex>
+                        <Grid templateColumns="repeat(3, 1fr)" gap={5}>
+                            {notes?.data?.map((item) => (
+                                <GridItem>
+                                    <Card>
+                                        <CardHeader>
+                                            <Heading>{item?.title}</Heading>
+                                        </CardHeader>
+                                        <CardBody>
+                                            <Text>{item?.description}</Text>
+                                        </CardBody>
+                                        <CardFooter justify="space-between" flexWrap="wrap">
+                                            <Button
+                                                onClick={() => router.push(`/notes/edit/${item?.id}`)}
+                                                flex="1"
+                                                variant="ghost"
+                                            >
+                                                Edit
+                                            </Button>
+                                            <Button
+                                                flex="1"
+                                                colorScheme="red"
+                                            >
+                                                Delete
+                                            </Button>
+                                        </CardFooter>
+                                    </Card>
+                                </GridItem>
+                            ))}
+                        </Grid>
+                    </Flex>
+                </Box>
+            </LayoutComponent>
+        </>
+    );
+}
